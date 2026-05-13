95d5dcd7dd600e69be6828569c19c0a3ee962d5c
MENYOTHYCODE <menyomentor@gmail.com>
2026-05-13 14:19:07 +0100

    views added

--- blog/form.py
+++ blog/form.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Post
+import django.contrib.auth.forms as UserCreationForm
+from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -27,3 +29,16 @@ class PostForm(forms.ModelForm):
             raise forms.ValidationError('Slug should not be part of the title.')
         return cleaned
         
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}))
+    
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+        
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
